1c2e0197f417179c90fd0434339ca131a8e65b20
Owen Auch <owenauch@wustl.edu>
2017-12-31 14:48:55 -0500

    all serializers done

--- fantasysoccer/api/serializers.py
+++ fantasysoccer/api/serializers.py
@@ -1,10 +1,39 @@
-from rest_framework import serializers
+from rest_framework import seyerializers
 from api.models import *
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = '__all__'
 
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
-        fields = ('id', 'team', 'name', 'country_code',
-            'position', 'date_of_birth')
+        fields = '__all__'
+
+class MatchStatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MatchStat
+        fields = '__all__'
+
+class LeagueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = League
+        fields = '__all__'
+
+class ClubSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = '__all__'
+
+class MatchweekSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matchweek
+        fields = '__all__'
+
+class RosterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Roster
+        fields = '__all__'
 
 
--- fantasysoccer/api/views.py
+++ fantasysoccer/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import *
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+
