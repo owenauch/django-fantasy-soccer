aaa193985d11779fef783589dc76ac38e05dd440
Owen Auch <owenauch@wustl.edu>
2018-01-24 13:33:52 +0000

    cors allowed

--- fantasysoccer/fantasysoccer/settings.py
+++ fantasysoccer/fantasysoccer/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_crontab',
-    'api'
+    'api',
+    'corsheaders'
 ]
 
 CRONJOBS = [
@@ -49,12 +50,18 @@ CRONJOBS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000'
+)
 
 ROOT_URLCONF = 'fantasysoccer.urls'
 
