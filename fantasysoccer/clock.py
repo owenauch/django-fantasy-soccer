40a04c8a7984d574853c2d855d8eeea6ad3627e5
Owen Auch <owenauch@wustl.edu>
2018-01-29 15:15:25 +0000

    finally works

--- fantasysoccer/clock.py
+++ fantasysoccer/clock.py
@@ -3,7 +3,7 @@ import get_match_stats
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', hours=6)
 def timed_job():
     get_match_stats.get_match_stats()
 
