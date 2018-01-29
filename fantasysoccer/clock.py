f7409a8e7be437bf2292f1b2545d549e65ec98d2
Owen Auch <owenauch@wustl.edu>
2018-01-29 15:10:05 +0000

    add ./

--- fantasysoccer/clock.py
+++ fantasysoccer/clock.py
@@ -5,6 +5,6 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    os.system('get_match_stats.py')
+    os.system('./get_match_stats.py')
 
 sched.start()
