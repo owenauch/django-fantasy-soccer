7a7971e692dac34e915df9847178a1ebbcd556f9
Owen Auch <owenauch@wustl.edu>
2018-01-30 13:05:43 +0000

    switched to postgres

--- fantasysoccer/fantasysoccer/settings.py
+++ fantasysoccer/fantasysoccer/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'fantasysoccer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dilly_league_db',
+        'USER': 'dilly_user',
+        'PASSWORD': 'dillydilly',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
