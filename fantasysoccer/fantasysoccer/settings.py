02af2df92033c36a5bed82c7e4ba9d8234720ccf
Owen Auch <owenauch@wustl.edu>
2018-01-30 13:28:22 +0000

    dj_database

--- fantasysoccer/fantasysoccer/settings.py
+++ fantasysoccer/fantasysoccer/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
