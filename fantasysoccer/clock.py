2f289791584e37dd601f51bff00ca6ca821dea90
Owen Auch <owenauch@wustl.edu>
2018-01-31 23:29:03 +0000

    add datetime argument to clock

--- fantasysoccer/clock.py
+++ fantasysoccer/clock.py
@@ -1,11 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import get_match_stats
+import datetime
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print("Running match stat collection job")
-    get_match_stats.get_match_stats()
+    get_match_stats.get_match_stats(datetime.datetime.today().strftime('%Y-%m-%d'))
 
 sched.start()
